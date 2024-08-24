eb90fa813a84e6a16bd7dc510a76390ed632064f
Jaycee-9 <thakkurjay2@gmail.com>
2024-08-24 18:49:30 +0530

    made changes for the routes to get otp and loginm the user

--- server/controller/user.js
+++ server/controller/user.js
@@ -2,26 +2,27 @@ import pool from "../database/database.js";
 
 export const userLogin = async (req, res) => {
   try {
-    const mobile_number = req.body.mobileNumber;
+    const mobile_number = req.body.mobile_number;
 
-    // Step 1: Check if the user already exists by querying the database
+    if (!mobile_number) {
+      return res.status(400).json({ message: "Mobile number is required" });
+    }
+
     const checkUserQuery = `SELECT * FROM users WHERE mobile_number = $1`;
     const checkUserResult = await pool.query(checkUserQuery, [mobile_number]);
 
     if (checkUserResult.rows.length > 0) {
-      // If user already exists, return the existing user info
       return res.status(200).json({
         message: "User already exists",
         user: checkUserResult.rows[0],
       });
     }
 
-    // Step 2: If the user does not exist, proceed to insert the new user
     const insertSTMT = `INSERT INTO users (mobile_number) VALUES ($1) RETURNING *`;
     const result = await pool.query(insertSTMT, [mobile_number]);
 
     res.status(201).json({
-      message: "User registered successfully",
+      message: "User logged in successfully",
       user: result.rows[0],
     });
   } catch (error) {
